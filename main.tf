9cbb06d78da12004d47bf52070005d81047a1500
ilopez64 <stalop2009@gmail.com>
2021-02-23 11:19:01 -0500

    edited main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ variable "aws_region" {
   type = string
   default = "us-east-1"
 }
-
+ 
 data "archive_file" "placeholder" {
   type = "zip"
   output_path = "${path.module}/lambda-function-payload.zip"

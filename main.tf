e704279cd06a60620d79ff0ca3036ebae8f767d9
ilopez64 <stalop2009@gmail.com>
2021-02-22 20:27:40 -0500

    edited tf files

--- main.tf
+++ main.tf
@@ -10,8 +10,6 @@ data "archive_file" "zip" {
   output_path = "${path.module}/lambda-function-payload.zip"
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
